a21bb182425d6bb639057ae570241a82f2319ee1
unknown <nico.fcok@gmail.com>
2023-07-18 10:09:46 -0300

    se realizan validaciones para el form modal patch y post menu, falta testear post menu

--- src/Components/ButtonPostMenu/ButtonPostMenu.js
+++ src/Components/ButtonPostMenu/ButtonPostMenu.js
@@ -4,7 +4,10 @@ import React, { useState } from 'react'
 const ButtonPostMenu = () => {
   
   const [update, setUpdate] = useState({})
-  
+  const [confirm, setConfirm] = useState(false)
+  const URL = 'http://localhost:8000'
+
+
   const handleChange = (e) => {
     const { name, value } = e.target
     setUpdate({
@@ -15,12 +18,22 @@ const ButtonPostMenu = () => {
 
 
   const posteaMenu = async () => {
-  let confirma = window.confirm('Desea postear el Menu?');
-  if (confirma) {
-    await axios.post('http://localhost:8000/post-menu', update);
-    window.location = 'http://localhost:3000/prueba'
+    try {
+      e.preventDefault()
+      let confirma = window.confirm('Desea postear el Menu?');
+      if (confirma) {
+        await axios.post(`${URL}/post-menu`, update);
+        setConfirm(confirma)
+          setTimeout(() => {
+            setConfirm(false)
+          }, 7000);
+      } else {
+        setConfirm(confirma)
+      }      
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
 
   return (
     <>
@@ -31,45 +44,48 @@ const ButtonPostMenu = () => {
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <h1 className="modal-title fs-5" id="exampleModalLabel">Postear nuevo menu</h1>
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div className="modal-body">
-            <form className='container'>
-             {/* <div className="mb-3">
-                <label htmlFor="lbl-id" className="form-label">ID</label>
-                <input type="text" id="lbl-id" className="form-control" value={_id} name='id' onChange={handleChange} disabled readOnly/>
-              </div> */}
+            <form className='container' onSubmit={posteaMenu}>
               <div className="mb-3">
                 <label htmlFor="lbl-nombre" className="form-label">Nombre</label>
-                <input type="text" className="form-control" id="lbl-nombre" placeholder="Ingrese nombre de la comida" name='nombre' onChange={handleChange}/>
+                <input type="text" className="form-control" id="lbl-nombre" placeholder="Ingrese nombre de la comida" name='nombre' pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$" title='Agregue el titulo con formato adecuado.' required onChange={handleChange}/>
               </div>
               <div className="mb-3">
                 <label htmlFor="lbl-estado" className="form-label">Estado</label>
-                <input type="text" className="form-control" id="lbl-estado" placeholder="Ingrese descripcion" name='estado' onChange={handleChange}/>
+                <input type="text" className="form-control" id="lbl-estado" placeholder="Ingrese descripcion" name='estado' required onChange={handleChange}/>
               </div>
               <div className="mb-3">
                 <label htmlFor="lbl-precio" className="form-label">Precio</label>
-                <input type="text" className="form-control" id="lbl-precio" placeholder="Ingrese precio" name='precio' onChange={handleChange}/>
+                <input type="text" className="form-control" id="lbl-precio" placeholder="Ingrese precio" name='precio' pattern="[0-9]{4}" title='Solo numeros de 4 cifras' minLength={1} maxLength={4} required onChange={handleChange}/>
               </div>
               <div className="mb-3">
                 <label htmlFor="lbl-desc" className="form-label">Descripcion</label>
-                <input type="text" className="form-control" id="lbl-desc" placeholder="Ingrese precio" name='detalle' onChange={handleChange}/>
+                <input type="text" className="form-control" id="lbl-desc" placeholder="Ingrese precio" name='detalle' maxLength={150} required onChange={handleChange}/>
               </div>
               <div className="mb-3">
                 <label htmlFor="lbl-cat" className="form-label">Categoria</label>
-                <input type="text" className="form-control" id="lbl-cat" placeholder="Ingrese precio" name='categoria' onChange={handleChange}/>
+                  <input type="text" className="form-control" id="lbl-cat" placeholder="Ingrese precio" name='categoria' required onChange={handleChange} />
+                  {/* Cambiar por select */}
               </div>
               <div className="mb-3">
                 <label htmlFor="lbl-img" className="form-label">Imagen</label>
-                <input type="text" className="form-control" id="lbl-img" placeholder="Ingrese solo url de la img" name='img' onChange={handleChange}/>
+                <input type="text" className="form-control" id="lbl-img" placeholder="Ingrese solo url de la img" name='img' required onChange={handleChange}/>
               </div>
-            </form>
-          </div>
           <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={posteaMenu}>Enviar</button>
+            {
+              confirm ? (
+                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              ) : (                
+                <button type="submit" className="btn btn-secondary">Enviar</button>                 
+              )
+            }
           </div>
+            </form>
         </div>
+          </div>
       </div>
     </div>
   </>
--- src/Components/ModalMenu/ModalMenu.js
+++ src/Components/ModalMenu/ModalMenu.js
@@ -1,42 +1,43 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-// import { useForm } from 'react-hook-form';
-
 
 const ModalMenu = ({_id}) => {
 
   const [update, setUpdate] = useState({})
   const [confirm, setConfirm] = useState(false)
-  
-  // const { register, handleSubmit, formState: { errors } } = useForm();  
-  // const patternName = /^[A-Za-z]+$/i;
+  const URL = ''  
 
-
-
-const handleChange = (e) => {
-    const { name, value } = e.target
-    setUpdate({
-      ...update,
-      id: _id,
-      [name]:value
-    })
+  const handleChange = (e) => {
+    try {
+      const { name, value } = e.target
+      setUpdate({
+        ...update,
+        id: _id,
+        [name]:value
+      }) 
+    } catch (error) {
+      console.log(error);
+    }
   }
   
 
-const actualizaMenu = async () => {
-  
-  let confirma = window.confirm('Desea actualizar el Menu?');
-    if (confirma) {
-      await axios.patch('http://localhost:8000/patch-menu', update);
-      setConfirm(confirma)
-      setTimeout(() => {
-        setConfirm(false)
-      }, 7000);
-  } else {
-    setConfirm(confirma)
+  const actualizaMenu = async (e) => {
+    try {
+      e.preventDefault()
+      let confirma = window.confirm('Desea actualizar el Menu?');
+      if (confirma) {
+        await axios.patch('http://localhost:8000/patch-menu', update);
+        setConfirm(confirma)
+        setTimeout(() => {
+          setConfirm(false)
+        }, 7000);
+      } else {
+        setConfirm(confirma)
+      }     
+    } catch (error) {
+      console.log(error);
+    }
   }
-  }
-  
 
   return (
     <div className="modal fade" id={_id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -55,37 +56,39 @@ const actualizaMenu = async () => {
                   </>
                 ) : (
                   <>
-                 <form className='container needs-validation' onSubmit={actualizaMenu} noValidate>
+                  <p className='small fst-italic text-end'>*Recuerde que puede cambiar un solo campo si lo desea.</p>
+                 <form className='container' onSubmit={actualizaMenu}>
                       <div className="mb-3">
                 <label htmlFor="formGroupExampleInput" className="form-label">Nombre</label>
-                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Ingrese nombre de la comida" name='newNom' required onChange={handleChange}/>
+                <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Ingrese nombre de la comida" pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$" title='Al menos ingresa el nombre en formato adecuado' required maxLength={25}
+                 name='newNom' onChange={handleChange}/>
               </div>
               <div className="mb-3">
                 <label htmlFor="formGroupExampleInput2" className="form-label">Descripcion</label>
-                <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Ingrese descripcion" name='newDesc' required onChange={handleChange}/>
+                <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Ingrese descripcion" name='newDesc' maxLength={150} onChange={handleChange}/>
               </div>
               <div className="mb-3">
                 <label htmlFor="formGroupExampleInput3" className="form-label">Precio</label>
-                <input type="text" className="form-control" id="formGroupExampleInput3" placeholder="Ingrese precio" name='newPre' required onChange={handleChange}/>
+                  <input type="text" className="form-control" id="formGroupExampleInput3" placeholder="Solo numeros de cuatro cifras" name='newPre' pattern="[0-9]{4}" title='Solo numeros de 4 cifras' minLength={1} maxLength={4} onChange={handleChange}/>
               </div>
               <div className="mb-3">
                 <label htmlFor="formGroupExampleInput4" className="form-label">Imagen</label>
-                <input type="text" className="form-control" id="formGroupExampleInput4" placeholder="Ingrese solo url de la img" name='newImg' required onChange={handleChange}/>
+                <input type="text" className="form-control" id="formGroupExampleInput4" placeholder="Ingrese solo url de la imagen" name='newImg' onChange={handleChange}/>
               </div>
+            <div className="modal-footer">
+            {
+              confirm ? (
+                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              ) : (                
+                <button type="submit" className="btn btn-secondary">Enviar</button>                 
+              )
+            }
+          </div>
+                      
             </form>          
               </>
                 )
               }
-          </div>
-          <div className="modal-footer">
-            {
-              confirm ? (
-                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-              ) : (
-                // <button type="button" className="btn btn-secondary" onClick={actualizaMenu} >Enviar</button>                 
-                <button type="submit" className="btn btn-secondary">Enviar</button>                 
-              )
-            }
           </div>
         </div>
       </div>
